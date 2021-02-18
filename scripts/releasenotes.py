b351092eed2462d2c0acbba55bb23ca286888d50
Kris Gesling <kris.gesling@mycroft.ai>
2021-02-18 11:51:53 +0930

    Auto-format

--- scripts/releasenotes.py
+++ scripts/releasenotes.py
@@ -22,22 +22,23 @@ import sys
 import github
 
 
-repo_name = sys.argv[1] if len(sys.argv) > 1 else 'MycroftAI/mycroft-core'
-gh_login = github.Github(os.environ['GITHUB_TOKEN'])
+repo_name = sys.argv[1] if len(sys.argv) > 1 else "MycroftAI/mycroft-core"
+gh_login = github.Github(os.environ["GITHUB_TOKEN"])
 repo = gh_login.get_repo(repo_name)
 
 
 labels = repo.get_labels()
-bugfix_labels = [l for l in labels if 'bug' in l.name.lower()]
-feature_labels = [l for l in labels if 'enhancement' in l.name.lower()]
-test_labels = features = [l for l in labels if 'test' in l.name.lower()]
+bugfix_labels = [l for l in labels if "bug" in l.name.lower()]
+feature_labels = [l for l in labels if "enhancement" in l.name.lower()]
+test_labels = features = [l for l in labels if "test" in l.name.lower()]
 
 
 def print_pr_info(pr):
-    print('- {} (#{}) by @{}'.format(pr.title, pr.number, pr.user.login))
+    print("- {} (#{}) by @{}".format(pr.title, pr.number, pr.user.login))
+
 
 def print_author_link(author):
-    print('- [@{}](https://github.com/{})'.format(author, author))
+    print("- [@{}](https://github.com/{})".format(author, author))
 
 
 class ReleaseNotes:
@@ -65,18 +66,18 @@ class ReleaseNotes:
         self.other.append(pr)
 
     def __len__(self):
-        return (len(self.features) +
-                len(self.bugfixes) +
-                len(self.tests) +
-                len(self.other))
+        return (
+            len(self.features) + len(self.bugfixes) + len(self.tests) + len(self.other)
+        )
 
 
 release_notes = ReleaseNotes()
 # Latest release as reference
 latest_release = repo.get_releases()[0]
 
-pull_requests = repo.get_pulls(state='closed', sort='updated',
-                               direction='desc', base=repo.default_branch)
+pull_requests = repo.get_pulls(
+    state="closed", sort="updated", direction="desc", base=repo.default_branch
+)
 
 counter = 0  # This is a bad way to handle comments to long closed PR's
 for pr in pull_requests:
@@ -89,39 +90,41 @@ for pr in pull_requests:
             break
 
 # Print all the different things
-print('# {}'.format(repo_name))
+print("# {}".format(repo_name))
 
 if release_notes.features:
-    print('\n\n## Features')
+    print("\n\n## Features")
     for pr in release_notes.features:
         print_pr_info(pr)
 
 if release_notes.bugfixes:
-    print('\n\n## Bugfixes')
+    print("\n\n## Bugfixes")
     for pr in release_notes.bugfixes:
         print_pr_info(pr)
 
 
 if release_notes.tests:
-    print('\n\n## Tests')
+    print("\n\n## Tests")
     for pr in release_notes.tests:
         print_pr_info(pr)
 
 if release_notes.other:
-    print('\n\n## Other changes')
+    print("\n\n## Other changes")
     for pr in release_notes.other:
         print_pr_info(pr)
 
 if release_notes.authors:
-    print('\n\n## Contributors list')
+    print("\n\n## Contributors list")
     for author in release_notes.authors:
         print_author_link(author)
 
-print('\n\n## Numbers:')
-print('\tNumber of authors contributing to '
-      'this release: {}'.format(len(release_notes.authors)))
-print('\tFeatures: {}'.format(len(release_notes.features)))
-print('\tBugfixes: {}'.format(len(release_notes.bugfixes)))
-print('\tTests: {}'.format(len(release_notes.tests)))
-print('\tOther: {}'.format(len(release_notes.other)))
-print('Total: {}'.format(len(release_notes)))
+print("\n\n## Numbers:")
+print(
+    "\tNumber of authors contributing to "
+    "this release: {}".format(len(release_notes.authors))
+)
+print("\tFeatures: {}".format(len(release_notes.features)))
+print("\tBugfixes: {}".format(len(release_notes.bugfixes)))
+print("\tTests: {}".format(len(release_notes.tests)))
+print("\tOther: {}".format(len(release_notes.other)))
+print("Total: {}".format(len(release_notes)))
